ba2f6c2be67c79ba18a778d4347cb666d96abe03
nienkebos <nienkebos@live.com>
2016-09-13 22:34:38 +0200

    ExhibitionsContainer+ReadMe update

--- src/ExhibitionsContainer/Exhibition.js
+++ src/ExhibitionsContainer/Exhibition.js
@@ -5,19 +5,9 @@ import jQuery from 'jquery'
 class Exhibition extends React.Component {
     constructor() {
         super();
-
-
     }
-
-  componentDidMount() {
-      this.setState({
-        courses: [{name: 'React', description: 'Its a course'}, {name: 'Javascript', description: 'Learn the basics'}],
-      });
-    }
-
 
     render() {
-
         return (
           <div>
             <h3>
