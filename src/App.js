83eed4f4865c2cce682bad061a609e0bc50b2e07
nienkebos <nienkebos@live.com>
2016-09-13 20:59:36 +0200

    First try with styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
 import React from 'react';
+
+//COMPONENTS//
 import ExhibitionForm from './ExhibitionForm';
 import Exhibitions from './Exhibitions';
+
+//STYLES//
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import BaseTheme from './styles/baseTheme'
+import {GridList, GridTile} from 'material-ui/GridList';
+import { Grid, Row, Cell } from 'react-inline-grid';
+
+
+
+// const styles = {
+//   root: {
+//     display: 'flex',
+//     flexWrap: 'wrap',
+//     justifyContent: 'space-around',
+//   },
+//   gridList: {
+//     width: 500,
+//     height: 500,
+//     overflowY: 'auto',
+//     marginBottom: 24,
+//   },
+// };
 
 
 class App extends React.Component {
 
 
 
+
     render() {
         return (
+          <Grid>
           <div>
-            <div>
-              <Exhibitions />
-            </div>
-            <div>
-              <ExhibitionForm />
-            </div>
+            <Row is="around">
+            <Cell is="4">
+              <div>
+                <Exhibitions />
+              </div>
+              </Cell>
+              <Cell is="2">
+
+              <div>
+                <ExhibitionForm />
+              </div>
+              </Cell>
+              </Row>
           </div>
+          </Grid>
         );
     }
 }
--- src/ExhibitionForm.js
+++ src/ExhibitionForm.js
@@ -1,38 +1,85 @@
 import React from 'react';
 import jQuery from 'jquery'
 
+//STYLES//
+import TextField from 'material-ui/TextField'
+import RaisedButton from 'material-ui/RaisedButton';
+
+
+const buttonStyle = {
+  float: 'right',
+  marginLeft: '2rem',
+}
 
 class ExhibitionForm extends React.Component {
 
-    createExhibition(){
-      
+    loadData() {
+      jQuery.get("http://localhost:3001/exhibitions.json", (function(data){
+        this.setState({
+          exhibitions: data.exhibitions,
+        });
+      }).bind(this));
+    }
+
+    createExhibition(e){
+      e.preventDefault();
+
+          let newExhibition = {
+            title: this.refs.title.value,
+            description: this.refs.description.value
+          };
+
+          jQuery.ajax({
+            type: "POST",
+            url: "http://localhost:3001/exhibitions.json",
+            data: JSON.stringify({
+              exhibition: newExhibition
+            }),
+            contentType: "application/json",
+            dataType: "json"
+
+          }).done(function( data ) {
+            alert( "Data saved: " + data );
+          })
+          .fail(function(error) {
+            console.log(error);
+          });
     }
 
     render() {
         return (
           <form onSubmit={this.createExhibition.bind(this)}>
-            <input
-              type="text"
-              className="form-control"
-              ref="title"
-              placeholder="What is the title of the exhibition?"
-            />
-            <input
-              type="text"
-              className="form-control"
-              ref="date"
-              placeholder="When is the exhibition planned?"
-            />
-            <textarea
-              className="form-control"
-              ref="description"
-              placeholder="Describe the exhibition..">
-            </textarea>
-            <button
+            <div>
+              <TextField
+                type="title"
+                className="form-control"
+                ref="title"
+                hintText="What is the title of the exhibition?" />
+            </div>
+
+            <div>
+              <TextField
+                type="date"
+                className="form-control"
+                ref="date"/>
+            </div>
+
+            <div>
+              <TextField
+                type="text"
+                className="form-control"
+                ref="description"
+                hintText="Describe the exhibition.."
+                multiLine={true}
+                rows={2}
+                rowsMax={30}/>
+            </div>
+
+            <RaisedButton
               type="submit"
               className="btn btn-primary">
               Create Exhibition
-            </button>
+            </RaisedButton>
           </form>
 
         );
